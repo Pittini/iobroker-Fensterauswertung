84dd0b8ba95816d064dbff96c3241bc24469723b
Pittini1 <pittini@web.de>
2020-03-16 15:11:12 +0100

    Bugfix at Rooms with more then one Windows

--- Fensterauswertung.js
+++ Fensterauswertung.js
@@ -1,3 +1,4 @@
+// V1.1.0 vom 16.3.2020
 //Script um offene Fenster pro Raum und insgesamt zu zählen. Legt pro Raum zwei Datenpunkte an, sowie zwei Datenpunkte fürs gesamte.
 //Möglichkeit eine Ansage nach x Minuten einmalig oder zyklisch bis Fensterschließung anzugeben
 
@@ -35,12 +36,17 @@ function Meldung(msg) {
 let OpenWindowCount = 0; // Gesamtzahl der geöffneten Fenster
 let RoomOpenWindowCount = []; // Array für offene Fenster pro Raum
 let OpenWindowMsgHandler = []; // Objektarray für timeouts pro Raum
-let Sensor = [] //Sensoren als Array anlegen
+let Sensor = []; //Sensoren als Array anlegen
+let SensorVal = [];//Sensorwerte als Array anlegen
 let Laufzeit = []; //Timer Laufzeit pro Fenster
+let RoomList = [];
+let z = 0;
+let DpCount = 0;
+let States = [];
 
-var Funktionen = getEnums('functions');
-for (var x in Funktionen) {        // loop ueber alle Functions
-    var Funktion = Funktionen[x].name;
+let Funktionen = getEnums('functions');
+for (let x in Funktionen) {        // loop ueber alle Functions
+    let Funktion = Funktionen[x].name;
 
     if (Funktion == undefined) {
         log("Keine Funktion gefunden");
@@ -50,13 +56,21 @@ for (var x in Funktionen) {        // loop ueber alle Functions
         var members = Funktionen[x].members;
         if (Funktion == WelcheFunktionVerwenden) { //Wenn Function ist Verschluss
             for (let y in members) { // Loop über alle Verschluss Members
+
                 Sensor[y] = members[y];
                 let room = getObject(Sensor[y], 'rooms').enumNames[0];
                 if (typeof room == 'object') room = room.de;
-                //Datenpunkte pro Raum anlegen
-                createState(praefix + room + ".RoomOpenWindowCount", 0, false, { read: true, write: true, name: "Anzahl der geöffneten Fenster im Raum", type: "number", def: 0 });
-                createState(praefix + room + ".IsOpen", false, false, { read: true, write: true, name: "Fenster offen?", type: "boolean", role: "state", def: false }); //
+                //Datenpunkte pro Raum vorbereiten
+                States[DpCount] = { id: praefix + room + ".RoomOpenWindowCount", initial: 0, forceCreation: false, common: { read: true, write: true, name: "Anzahl der geöffneten Fenster im Raum", type: "number", def: 0 } };
+                DpCount++;
+                States[DpCount] = { id: praefix + room + ".IsOpen", initial: false, forceCreation: false, common: { read: true, write: true, name: "Fenster offen?", type: "boolean", role: "state", def: false } }; //
+                DpCount++;
                 //log(Funktion + ': ' + room);
+                if (RoomList.indexOf(room) == -1) { //Raumliste ohne Raumduplikate erzeugen
+                    RoomList[z] = room
+                    if (logging) log(RoomList[z])
+                    z++
+                };
                 RoomOpenWindowCount[y] = 0; // Array mit 0 initialisieren
                 Laufzeit[y] = 0; // Array mit 0 initialisieren
             };
@@ -65,21 +79,41 @@ for (var x in Funktionen) {        // loop ueber alle Functions
 };
 
 //Struktur anlegen in js.0 um Sollwert und Summenergebniss zu speichern
-//Generischen Datenpunkt anlegen 
-createState(praefix + "AlleFensterZu", true, false, { read: true, write: true, name: "Fenster zu?", type: "boolean", role: "state", def: true }); //
-createState(praefix + "WindowsOpen", 0, false, { read: true, write: true, name: "Anzahl der geöffneten Fenster", type: "number", def: 0 });
+//Generische Datenpunkte vorbereiten 
+States[DpCount] = { id: praefix + "AlleFensterZu", initial: true, forceCreation: false, common: { read: true, write: true, name: "Fenster zu?", type: "boolean", role: "state", def: true } }; //
+DpCount++;
+States[DpCount] = { id: praefix + "WindowsOpen", initial: 0, forceCreation: false, common: { read: true, write: true, name: "Anzahl der geöffneten Fenster", type: "number", def: 0 } };
 
-let SensorVal = []; //SensorDatenpunkten Array zuweisen
+//Alle States anlegen, Main aufrufen wenn fertig
+let numStates = States.length;
+States.forEach(function (state) {
+    createState(state.id, state.initial, state.forceCreation, state.common, function () {
+        numStates--;
+        if (numStates === 0) {
+            if (logging) log("CreateStates fertig!");
+            main();
+        };
+    });
+});
 
-for (let x = 0; x < Sensor.length; x++) {
-    SensorVal[x] = getState(Sensor[x]).val; // Wert von Sensor in Schleife einlesen
-    //log(SensorVal[x]);
+function main() {
+    for (let x = 0; x < Sensor.length; x++) {
+        SensorVal[x] = getState(Sensor[x]).val; // Wert von Sensor in Schleife einlesen
+        //log(SensorVal[x]);
+    };
+
+    CreateTrigger();
+    CheckAllWindows(); //Bei Scriptstart alle Fenster einlesen
+
 };
 
+
+
 function GetRoom(x) { // Liefert den Raum von Sensor x
+    if (logging) log("Reaching GetRoom x=" + x)
     let room = getObject(Sensor[x], 'rooms').enumNames[0];
     if (room == undefined) {
-        log("Kein Raum definiert");
+        log("Kein Raum definiert", 'error');
         return "Kein Raum definiert";
     };
     if (typeof room == 'object') room = room.de;
@@ -87,48 +121,57 @@ function GetRoom(x) { // Liefert den Raum von Sensor x
 };
 
 function CheckWindow(x) { //Für einzelnes Fenster. Via Trigger angesteuert.
+    let TempRoom = GetRoom(x); //Raum des aktuellen Sensors bestimmen
+    let TempRoomIndex = RoomList.indexOf(TempRoom); // Raumlistenindex für aktuellen Raum bestimmen
     //log(getObject(SensorPraefix+Sensor[x], 'rooms').enumNames.join(', '));
-    if (logging) log(SensorVal[x])
+    if (logging) log("reaching CheckWindow, SensorVal[" + x + "]=" + SensorVal[x] + " TempRoom=" + TempRoom)
     if (SensorVal[x] == true || SensorVal[x] == "offen" || SensorVal[x] == "gekippt" || SensorVal[x] == "open" || SensorVal[x] == "tilted") { //Fenster is offen
         Laufzeit[x] = 0;
         OpenWindowCount = OpenWindowCount + 1;
-        RoomOpenWindowCount[x] = getState(praefix + GetRoom(x) + ".RoomOpenWindowCount").val + 1;
-        //log(RoomOpenWindowCount[x])
+        RoomOpenWindowCount[TempRoomIndex] = getState(praefix + TempRoom + ".RoomOpenWindowCount").val + 1;
+        if (logging) log("RoomOpenWindowCount für " + TempRoom + "=" + RoomOpenWindowCount[TempRoomIndex])
         setState(praefix + "AlleFensterZu", false);
-        setState(praefix + GetRoom(x) + ".IsOpen", true);
+        setState(praefix + TempRoom + ".IsOpen", true);
         setState(praefix + "WindowsOpen", OpenWindowCount);
-        setState(praefix + GetRoom(x) + ".RoomOpenWindowCount", RoomOpenWindowCount[x]);
+        setState(praefix + TempRoom + ".RoomOpenWindowCount", RoomOpenWindowCount[TempRoomIndex]);
 
-        //log(GetRoom(x) + " Fenster geöffnet");
+        if (logging) log(TempRoom + " Fenster geöffnet");
         if (UseEventLog == true) WriteEventLog(GetRoom(x) + " Fenster geöffnet!");
-        if (InfoMsgAktiv == true) {
-            if (RepeatInfoMsg == true) { // Wenn Intervallmeldung eingestellt Interval starten und Dauer bei Ansage aufaddieren
-                OpenWindowMsgHandler[x] = setInterval(function () {
-                    Laufzeit[x] = Laufzeit[x] + ZeitBisNachricht;
-                    Meldung(GetRoom(x) + "fenster seit " + Laufzeit[x] / 1000 / 60 + " Minuten geöffnet!");
-                }, ZeitBisNachricht);
-            }
-            else {
-                OpenWindowMsgHandler[x] = setTimeout(function () { // Wenn einmalige Meldung eingestellt
-                    Meldung(GetRoom(x) + "fenster seit " + ZeitBisNachricht / 1000 / 60 + " Minuten geöffnet!");
-                }, ZeitBisNachricht);
+        if (RoomOpenWindowCount[TempRoomIndex] == 1) {
+            if (InfoMsgAktiv == true) {
+                if (RepeatInfoMsg == true) { // Wenn Intervallmeldung eingestellt Interval starten und Dauer bei Ansage aufaddieren
+                    if (logging) log("Setting Interval to Room:" + TempRoom);
+
+                    OpenWindowMsgHandler[x] = setInterval(function () {
+                        Laufzeit[x] = Laufzeit[x] + ZeitBisNachricht;
+                        Meldung(TempRoom + "fenster seit " + Laufzeit[x] / 1000 / 60 + " Minuten geöffnet!");
+                    }, ZeitBisNachricht);
+                }
+                else { // Wenn einmalige Meldung eingestellt
+                    if (logging) log("Setting Timeout to Room:" + TempRoom);
+
+                    OpenWindowMsgHandler[x] = setTimeout(function () {
+                        Meldung(TempRoom + "fenster seit " + ZeitBisNachricht / 1000 / 60 + " Minuten geöffnet!");
+                    }, ZeitBisNachricht);
+                };
             };
         };
     }
     else {
         OpenWindowCount = OpenWindowCount - 1;
-        RoomOpenWindowCount[x] = getState(praefix + GetRoom(x) + ".RoomOpenWindowCount").val - 1;
+        //RoomOpenWindowCount[x] = getState(praefix + GetRoom(x) + ".RoomOpenWindowCount").val - 1;
+        RoomOpenWindowCount[TempRoomIndex] = getState(praefix + TempRoom + ".RoomOpenWindowCount").val - 1;
 
         setState(praefix + "WindowsOpen", OpenWindowCount);
-        setState(praefix + GetRoom(x) + ".RoomOpenWindowCount", RoomOpenWindowCount[x]);
+        setState(praefix + TempRoom + ".RoomOpenWindowCount", RoomOpenWindowCount[TempRoomIndex]);
 
-        log(GetRoom(x) + " Fenster geschlossen.");
+        log(TempRoom + " Fenster geschlossen.");
         if (UseEventLog == true) WriteEventLog(GetRoom(x) + " Fenster geschlossen!");
-        if (RoomOpenWindowCount[x] == 0) {
-            setState(praefix + GetRoom(x) + ".IsOpen", false);
+        if (RoomOpenWindowCount[TempRoomIndex] == 0) {
+            setState(praefix + TempRoom + ".IsOpen", false);
 
             if (RepeatInfoMsg == true) {
-                if (logging) log("reaching clearInterval - OpenWindowMsgHandler[x] = " + OpenWindowMsgHandler[x]);
+                if (logging) log("reaching clearInterval - [x] = " + [x]);
                 clearInterval(OpenWindowMsgHandler[x]);
             }
             else {
@@ -140,7 +183,7 @@ function CheckWindow(x) { //Für einzelnes Fenster. Via Trigger angesteuert.
 
         if (OpenWindowCount == 0) {
             setState(praefix + "AlleFensterZu", true);
-            setState(praefix + GetRoom(x) + ".IsOpen", false);
+            setState(praefix + TempRoom + ".IsOpen", false);
             log("Alle Fenster geschlossen.");
         };
     };
@@ -152,38 +195,47 @@ function CheckWindow(x) { //Für einzelnes Fenster. Via Trigger angesteuert.
 
 function CheckAllWindows() { //Prüft bei Programmstart alle Fenster
     for (let x = 0; x < Sensor.length; x++) { //Alle Sensoren durchlaufen
+        let TempRoom = GetRoom(x);
+        let TempRoomIndex = RoomList.indexOf(TempRoom);
+        //if (logging) log(TempRoom);
         if (SensorVal[x] == true || SensorVal[x] == "offen" || SensorVal[x] == "gekippt" || SensorVal[x] == "open" || SensorVal[x] == "tilted") { //Fenster is offen
             OpenWindowCount = OpenWindowCount + 1;
-            RoomOpenWindowCount[x] = RoomOpenWindowCount[x] + 1;
+            RoomOpenWindowCount[RoomList.indexOf(TempRoom)] = RoomOpenWindowCount[TempRoomIndex] + 1;
+            //RoomOpenWindowCount[x] = RoomOpenWindowCount[x] + 1;
+            log("Temproom=" + TempRoom + "TempRoomIndex=" + RoomList.indexOf(TempRoom) + " RommOpenWindowcount=" + RoomOpenWindowCount[TempRoomIndex]);
+
             setState(praefix + "AlleFensterZu", false);
             setState(praefix + "WindowsOpen", OpenWindowCount);
 
-            setState(praefix + GetRoom(x) + ".IsOpen", true);
-            setState(praefix + GetRoom(x) + ".RoomOpenWindowCount", RoomOpenWindowCount[x]);
-            if (InfoMsgAktiv == true) {
+            setState(praefix + TempRoom + ".IsOpen", true);
+            setState(praefix + TempRoom + ".RoomOpenWindowCount", RoomOpenWindowCount[TempRoomIndex]);
+            if (InfoMsgAktiv == true && RoomOpenWindowCount[RoomList.indexOf(TempRoom)] == 1) {
                 if (RepeatInfoMsg == true) { // Wenn Intervallmeldung eingestellt Interval starten und Dauer bei Ansage aufaddieren
+                    if (logging) log("Setting Interval at initialization to Room:" + TempRoom);
                     OpenWindowMsgHandler[x] = setInterval(function () {
                         Laufzeit[x] = Laufzeit[x] + ZeitBisNachricht;
-                        Meldung(GetRoom(x) + "fenster seit " + Laufzeit[x] / 1000 / 60 + " Minuten geöffnet!");
+                        Meldung(TempRoom + "fenster seit " + Laufzeit[x] / 1000 / 60 + " Minuten geöffnet!");
                     }, ZeitBisNachricht);
                 }
                 else {
+                    if (logging) log("Setting Timeout at initialization to Room:" + TempRoom);
+
                     OpenWindowMsgHandler[x] = setTimeout(function () { // Wenn einmalige Meldung eingestellt
-                        Meldung(GetRoom(x) + "fenster seit " + ZeitBisNachricht / 1000 / 60 + " Minuten geöffnet!");
+                        Meldung(TempRoom + "fenster seit " + ZeitBisNachricht / 1000 / 60 + " Minuten geöffnet!");
                     }, ZeitBisNachricht);
                 };
             };
 
-            //log(GetRoom(x) + " Fenster geöffnet");
+            log(TempRoom + " Fenster = geöffnet");
             //WriteEventLog(GetRoom(x) + " Fenster geöffnet!");
         }
         else {
-            RoomOpenWindowCount[x] = getState(praefix + GetRoom(x) + ".RoomOpenWindowCount").val - 1;
-            if (RoomOpenWindowCount[x] < 0) { RoomOpenWindowCount[x] = 0 };
-            setState(praefix + GetRoom(x) + ".IsOpen", false);
-            setState(praefix + GetRoom(x) + ".RoomOpenWindowCount", RoomOpenWindowCount[x]);
+            RoomOpenWindowCount[RoomList.indexOf(TempRoom)] = getState(praefix + TempRoom + ".RoomOpenWindowCount").val - 1;
+            if (RoomOpenWindowCount[TempRoomIndex] < 0) RoomOpenWindowCount[TempRoomIndex] = 0;
+            setState(praefix + TempRoom + ".IsOpen", false);
+            setState(praefix + TempRoom + ".RoomOpenWindowCount", RoomOpenWindowCount[TempRoomIndex]);
 
-            // log(GetRoom(x) + " Fenster geschlossen.");
+            log(TempRoom + " Fenster = geschlossen.");
             //WriteEventLog(GetRoom(x) + " Fenster geschlossen!");
         };
         //log(OpenWindowCount);
@@ -197,33 +249,33 @@ function CheckAllWindows() { //Prüft bei Programmstart alle Fenster
 
 };
 
-CheckAllWindows(); //Bei Scriptstart alle Fenster einlesen
-
-//Trigger für Sensoren erzeugen
-for (let x = 0; x < Sensor.length; x++) { //Alle Sensoren durchlaufen
-    on(Sensor[x], function (dp) { //Trigger in Schleife erstellen
-        if (dp.channelId.search(praefix) == -1) { //Ausschliessen dass das Scriptverzeichnis zum Triggern verwendet wird
-            SensorVal[x] = getState(Sensor[x]).val;
-            CheckWindow(x);
-        }
-        else {
-            log("Fehler, Datenpunkt im Scriptverzeichnis als Trigger definiert", "error");
-        };
-    });
-};
-
-onStop(function () { //Bei Scriptende alle Timer löschen
-    for (let x = 1; x < Sensor.length; x++) {
-        if (RoomOpenWindowCount[x] == 0) {
-            if (RepeatInfoMsg == true) {
-                clearInterval(OpenWindowMsgHandler[x]);
+function CreateTrigger() {
+    //Trigger für Sensoren erzeugen
+    for (let x = 0; x < Sensor.length; x++) { //Alle Sensoren durchlaufen
+        on(Sensor[x], function (dp) { //Trigger in Schleife erstellen
+            if (dp.channelId.search(praefix) == -1) { //Ausschliessen dass das Scriptverzeichnis zum Triggern verwendet wird
+                SensorVal[x] = dp.state.val;
+                //SensorVal[x] = getState(Sensor[x]).val;
+                CheckWindow(x);
             }
             else {
-                clearTimeout(OpenWindowMsgHandler[x]);
+                log("Fehler, Datenpunkt im Scriptverzeichnis als Trigger definiert", "error");
             };
-        };
-
+        });
     };
-}, 100);
 
+    onStop(function () { //Bei Scriptende alle Timer löschen
+        for (let x = 1; x < Sensor.length; x++) {
+            if (RoomOpenWindowCount[x] == 0) {
+                if (RepeatInfoMsg == true) {
+                    clearInterval(OpenWindowMsgHandler[x]);
+                }
+                else {
+                    clearTimeout(OpenWindowMsgHandler[x]);
+                };
+            };
 
+        };
+    }, 100);
+
+};
